e9f43640721ab49d888ea7a99de793a1a48b91b2
pentacle <pentacle@pentacle.xyz>
2024-02-14 22:32:24 +0000

    feat: second

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-export const viewport = {
-  width: 'device-width',
-  initialScale: 1.0,
-};
+// export const viewport = {
+//   width: 'device-width',
+//   initialScale: 1.0,
+// };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
